7e27844568860ff902db4cd459475646e7f5091e
Serg-Dmytruk <54394423+Serg-Dmytruk@users.noreply.github.com>
2022-02-12 15:03:34 +0200

    update

--- StepEbay.Main.Client.Common/Pages/SignIn.razor.cs
+++ StepEbay.Main.Client.Common/Pages/SignIn.razor.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Components;
+using StepEbay.Main.Client.Base.Layout;
+using StepEbay.Main.Common.Models.Auth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StepEbay.Main.Client.Common.Pages
+namespace StepEbay.Main.Client.Base.Pages
 {
     [Route("/signup")]
+    [Layout(typeof(EmptyLayout))]
     public partial class Auth
     {
         //TODO [Inject] private ITokenProvider TokenProvider { get; set; }
         [Inject] private NavigationManager _navigationManager { get; set; }
+
+        private SignInRequestDto _signInRequestDto { get; set; }
+        private bool _showPreloader { get; set; }
+        private bool _rememberMe { get; set; }
+
+        private async Task SigninRequest()
+        {
+            
+        }
     }
 }
--- StepEbay.Main.Client.Common/Pages/SignUp.razor.cs
+++ StepEbay.Main.Client.Common/Pages/SignUp.razor.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components;
+using StepEbay.Main.Client.Base.Layout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StepEbay.Main.Client.Common.Pages
+namespace StepEbay.Main.Client.Base.Pages
 {
     [Route("/signup")]
+    [Layout(typeof(EmptyLayout))]
     public partial class SignUp
     {
       
