6fc27c3a718a72414071ef5eabe837c53fb7f475
122tp12 <lazokivan0@gmail.com>
2022-02-20 09:47:43 +0200

    ,,

--- StepEbay.Api/Controllers/EmailController.cs
+++ StepEbay.Api/Controllers/EmailController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StepEbay.Main.Api.Common.Services.EmailSenderServices;
-using Swashbuckle.AspNetCore.Annotations;
 
 namespace StepEbay.Main.Api.Controllers
 {
@@ -16,18 +15,20 @@ namespace StepEbay.Main.Api.Controllers
         /// <summary>
         /// Відправлення новин з стандартного адресу
         /// </summary>
-        [HttpPost("SendNews")]
-        public async Task SendNews(string mail, string title, string description)
+        [HttpPost("news")]
+        public async Task SendNews(string title, string description)
         {
+            string mail = "";//get mail from session
             await _emailSenderService.SendEmail(mail, title, description);//TODO: content of mail must be changed
         }
 
         /// <summary>
         /// Відправлення інформації про ставку з стандартного адресу
         /// </summary>
-        [HttpPost("SendBetMessage")]
-        public async Task SendBetMessage(string mail, string title, string description)
+        [HttpPost("bet")]
+        public async Task SendBetMessage()
         {
+            string mail = "";//get mail from session
             await _emailSenderService.SendEmail(mail, title, description);//TODO: content of mail must be changed
         }
 
@@ -35,8 +36,9 @@ namespace StepEbay.Main.Api.Controllers
         /// Відправлення "підтвердження реєстрації" з стандартного адресу
         /// </summary>
         [HttpPost("SendRegistrationConfirm")]
-        public async Task SendRegistrationConfirm(string mail, string title, string description)
+        public async Task SendRegistrationConfirm()
         {
+            string mail = "";//get mail from session
             await _emailSenderService.SendEmail(mail, title, description);//TODO: content of mail must be changed
         }
     }
