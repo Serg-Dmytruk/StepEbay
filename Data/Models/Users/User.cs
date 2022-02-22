d5b199615403db2c5e41cbd9f977b516478fd726
122tp12 <lazokivan0@gmail.com>
2022-02-22 17:49:14 +0200

    Users migration

--- Data/Models/Users/User.cs
+++ Data/Models/Users/User.cs
@@ -3,14 +3,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace StepEbay.Data.Models.Users
 {
-    public class User : IDbServiceEntity<int>
-    {
-        [Key]
-        public int Id { get; set; }
-        public DateTime Created { get; set; }
-        public string FullName { get; set; }
-        public string NickName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+	public class User : IDbServiceEntity<int>
+	{
+		[Key]
+		public int Id { get; set; }
+		public DateTime Created { get; set; }
+		public string FullName { get; set; }
+		public string NickName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string? Adress { get; set; }
+	}
 }
