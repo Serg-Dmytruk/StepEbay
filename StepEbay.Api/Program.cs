13e440e13b88a486c29fc49e4847a673c1e459e8
Serg-Dmytruk <54394423+Serg-Dmytruk@users.noreply.github.com>
2022-05-29 14:08:46 +0300

    update

--- StepEbay.Api/Program.cs
+++ StepEbay.Api/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using StepEbay.Data;
 using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
 using StepEbay.Main.Api.Common.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
