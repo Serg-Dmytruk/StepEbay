b75ef5e32d046441ad8ae56d35b1f694a000b93e
Serg-Dmytruk <54394423+Serg-Dmytruk@users.noreply.github.com>
2022-05-29 14:35:18 +0300

    fix

---
+++ StepEbay.Main.Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using StepEbay.Main.Client.Common.Providers;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace StepEbay.Main.Client.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject] private ITokenProvider TokenProvider { get; set; }
+        private string UserName { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            var authState = await TokenProvider.GetAuthenticationStateAsync();
+            UserName = authState.User.Claims.Single(c => c.Type == "nickName").Value;
+
+        }
+
+        private async Task LogOut()
+        {
+
+        }
+    }
+}
