49312c3b7e8896c94ca6df4c832f9d8d73dbb39e
Serg-Dmytruk <54394423+Serg-Dmytruk@users.noreply.github.com>
2022-06-18 19:29:58 +0300

    update

---
+++ StepEbay.Admin.Client/Services/ApiService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Refit;
+using StepEbay.Admin.Client.Common.Providers;
+using StepEbay.Admin.Client.Common.RestServices;
+using StepEbay.Common.ApiServices;
+using StepEbay.Common.Lockers;
+using StepEbay.Common.Models.Auth;
+using StepEbay.Main.Common.Models.Auth;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StepEbay.Admin.Client.Services
+{
+    public class ApiService : DefaultApiService<IApiMethods>, IApiService
+    {
+        public ApiService(IConfiguration configuration, ITokenProvider authProvider, SemaphoreManager manager,
+             HttpClient httpClient)
+            : base(authProvider, manager.Get("API"), httpClient, configuration.GetConnectionString("Api"))
+        {
+
+        }
+
+        protected override async Task<(RefreshTokenData, HttpStatusCode)> Refresh(RefreshTokenData data)
+        {
+            ApiResponse<RefreshTokenResponseDto> response =
+                await ApiMethods.RefreshToken(new RefreshTokenRequestDto
+                {
+                    AccessToken = data.AccessToken,
+                    RefreshToken = data.RefreshToken
+                });
+
+            return new(new RefreshTokenData
+            {
+                AccessToken = response.Content?.AccessToken,
+                RefreshToken = response.Content?.RefreshToken
+            }, response.StatusCode);
+        }
+    }
+}
