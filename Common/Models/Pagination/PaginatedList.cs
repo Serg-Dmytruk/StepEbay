4ee316cff12dba5322cec45b7c793e3c682fc6f8
Serg-Dmytruk <54394423+Serg-Dmytruk@users.noreply.github.com>
2022-06-18 15:06:50 +0300

    add paginatedList model

---
+++ Common/Models/Pagination/PaginatedList.cs
@@ -0,0 +1,8 @@
+﻿namespace StepEbay.Common.Models.Pagination
+{
+    public class PaginatedList<T>
+    {
+        public List<T> List { get; set; }
+        public int CountAll { get; set; }
+    }
+}
