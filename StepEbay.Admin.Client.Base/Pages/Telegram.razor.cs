d770ec162d3e2ef4228ceb08bb7c6525ba2a5381
122tp12 <63251202+122tp12@users.noreply.github.com>
2022-06-19 14:24:34 +0300

    _

--- StepEbay.Admin.Client.Base/Pages/Telegram.razor.cs
+++ StepEbay.Admin.Client.Base/Pages/Telegram.razor.cs
@@ -11,9 +11,9 @@ namespace StepEbay.Admin.Client.Base.Pages
         private string? _inputValue;
         [Inject] IApiService _service { get; set; }
         
-        public void UpdateToken()
+        public async Task UpdateToken()
         {
-            _service.ExecuteRequest(()=>_service.ApiMethods.AddGroup(_inputValue));
+            await _service.ExecuteRequest(()=>_service.ApiMethods.AddGroup(_inputValue));
         }
     }
 }
