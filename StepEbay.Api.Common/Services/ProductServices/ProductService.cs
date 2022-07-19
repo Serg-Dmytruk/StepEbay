4607e19504b55ee8ddc01da7c2ffd199e2db5994
Volodymyr Yaruchyk <riko03030303@gmail.com>
2022-07-19 19:40:01 +0300

    product filters zagotovka

---
+++ StepEbay.Api.Common/Services/ProductServices/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using StepEbay.Common.Models.Pagination;
+using StepEbay.Data.Common.Services.ProductDbServices;
+using StepEbay.Main.Common.Models.Product;
+
+namespace StepEbay.Main.Api.Common.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private ProductDbService _productDb;
+        public ProductService(ProductDbService productDb)
+        {
+            _productDb = productDb;
+        }
+         
+        public async Task<PaginatedList<ProductDto>> GetProductList(ProductFilters filters)
+        {
+            var products = await _productDb.GetProductList(filters);
+
+            return new PaginatedList<ProductDto>
+            {
+                List = products.Select(x => new ProductDto { Title = x.Title }).ToList(),
+                CountAll = await _productDb.GetProductCount(filters)
+            };
+        }
+    }
+}
---
+++ StepEbay.Common/Models/Product/ProductFilters.cs
@@ -0,0 +1,10 @@
+﻿namespace StepEbay.Main.Common.Models.Product
+{
+    public class ProductFilters
+    {
+        public int state { get; set; }
+        public int category { get; set; }
+        public int priceStart { get; set; }
+        public int priceEnd { get; set; }
+    }
+}
