95ef7b5990db0c1097de9617e19166dc0f512e61
122tp12 <63251202+122tp12@users.noreply.github.com>
2022-08-01 14:28:44 +0300

    mid

---
+++ StepEbay.Common/Models/Person/PersonUpdateResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StepEbay.Main.Common.Models.Person
+{
+    public class PersonUpdateResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public List<string> FailuresList { get; set; }
+    }
+}
