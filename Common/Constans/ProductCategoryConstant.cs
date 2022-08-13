9ecb27bb9bbdf178070c4d4551bfe52a2ee2c317
Volodymyr Yaruchyk <riko03030303@gmail.com>
2022-08-13 20:37:39 +0300

    Constants

---
+++ Common/Constans/ProductCategoryConstant.cs
@@ -0,0 +1,17 @@
+﻿namespace StepEbay.Common.Constans
+{
+    public static class ProductCategoryConstant
+    {
+        public static readonly string ELECTRONIC = "electronic";
+        public static readonly string FASHION = "fashion";
+        public static readonly string HEALTHANDBUATY = "heals & buaty";
+        public static readonly string HOMEANDGARDEN = "home & garden";
+        public static readonly string FORGAMERS = "for gamers";
+        public static readonly string FORKIDS = "for kids";
+        public static readonly string SPORT = "sport";
+        public static readonly string FOOD = "food";
+        public static readonly string COLLECTIBLE = "collectible";
+        public static readonly string INDUSTRIAL = "industrial equipment";
+        public static readonly string AUTOANDPARTS = "auto and parts";
+    }
+}
