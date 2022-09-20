5fe04ccd3dba0fbb0504e475acaab9cf0d411878
Volodymyr Yaruchyk <riko03030303@gmail.com>
2022-09-20 18:14:16 +0300

    component signUp and category icons

---
+++ StepEbay.Main.Client.Base/Components/SignUp/SignUp.razor.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using StepEbay.Common.Models.RefitModels;
+using StepEbay.Main.Client.Base.Layout;
+using StepEbay.Main.Client.Common.DataValidationServices;
+using StepEbay.Main.Client.Common.RestServices;
+using StepEbay.Main.Common.Models.Auth;
+using System.Net;
+
+namespace StepEbay.Main.Client.Base.Components.SignUp
+{
+    [Layout(typeof(EmptyLayout))]
+    public partial class SignUp
+    {
+        [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] IApiService ApiService { get; set; }
+
+        private SignUpRequestDto SignUpRequestDto { get; set; } = new();
+        private bool ShowPreloader { get; set; } = true;
+
+        private Dictionary<string, List<string>> Errors = new();
+
+        private bool ShowModal { get; set; } = false;
+
+        private readonly List<string> RegMess = new() { "Вам надіслано лист для підтвердження реєстрації!" };
+
+        protected void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                ShowPreloader = false;
+                StateHasChanged();
+            }
+        }
+
+        private async Task SignUpRequest()
+        {
+            Errors.Clear();
+            ShowPreloader = true;
+            var validator = new AuthValidator();
+            var result = await validator.ValidateAsync(SignUpRequestDto);
+
+            if (!result.IsValid)
+            {
+                var list = new List<string>();
+                result.Errors.ForEach(error => list.Add(error.ToString()));
+                Errors.Add("Registration", list);
+            }
+            else
+            {
+                ResponseData<SignInResponseDto> response = await ApiService.ExecuteRequest(() => ApiService.ApiMethods.SignUp(SignUpRequestDto));
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    ShowModal = true;
+                    ShowPreloader = false;
+                    StateHasChanged();
+                    return;
+                }
+
+                Errors = response.Errors;
+            }
+
+            if (Errors.Count > 0)
+                ShowModal = true;
+
+            ShowPreloader = false;
+            StateHasChanged();
+        }
+
+        private void CloseModal(bool show)
+        {
+            ShowModal = show;
+            StateHasChanged();
+        }
+    }
+}
