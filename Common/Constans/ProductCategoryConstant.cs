4cbf1c4d614c54dc41084cb926a559d278a0974b
Volodymyr Yaruchyk <riko03030303@gmail.com>
2022-09-20 18:52:13 +0300

    categories changes

--- Common/Constans/ProductCategoryConstant.cs
+++ Common/Constans/ProductCategoryConstant.cs
@@ -7,4 +7,12 @@
         public static readonly string BUATY = "Краса";
         public static readonly string SPORT = "Спорт";
     }
+
+    public static class ProductCategoryIdConstant
+    {
+        public static readonly string TELEPHONE = "12";
+        public static readonly string CLOTH = "13";
+        public static readonly string BUATY = "15";
+        public static readonly string SPORT = "14";
+    }
 }
