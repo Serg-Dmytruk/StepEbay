143cad5ba04a1ea540c9120a24e9679c1a814ae4
122tp12 <63251202+122tp12@users.noreply.github.com>
2022-10-03 01:25:10 +0300

    початок bet-ів

--- StepEbay.Api/Controllers/BetController.cs
+++ StepEbay.Api/Controllers/BetController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StepEbay.Common.Models.RefitModels;
+using StepEbay.Data.Common.Services.BetsDbServices;
+using StepEbay.Main.Api.Common.Services.BetServices;
 
 namespace StepEbay.Main.Api.Controllers
 {
@@ -7,13 +10,18 @@ namespace StepEbay.Main.Api.Controllers
     [Route("bet")]
     public class BetController : ControllerBase
     {
+        public readonly IBetService _bets;
+
+        public BetController(IBetService bets)
+        {
+            _bets = bets;
+        }
 
         [HttpPost("place/{lotId}")]
-
-        public async Task PlaceBet(int lotId)
+        public async Task<ResponseData> PlaceBet(int lotId)
         {
-            var t = User.Claims.Single(c => c.Type == "nickName").Value; //claimType
-            //User.Identity.Name Id 
+            int userId = int.Parse(User.Claims.Single(c => c.Type == "nickName").Value);
+            return await _bets.PlaceBet(userId, lotId);
         }
     }
 }
