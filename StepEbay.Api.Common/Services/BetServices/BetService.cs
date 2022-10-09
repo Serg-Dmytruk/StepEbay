3f8bb93fe0f7eee97ac8d8fb9e4b9c230c0a2c1f
122tp12 <63251202+122tp12@users.noreply.github.com>
2022-10-09 21:03:28 +0300

    bet працює

--- StepEbay.Api.Common/Services/BetServices/BetService.cs
+++ StepEbay.Api.Common/Services/BetServices/BetService.cs
@@ -14,24 +14,31 @@ namespace StepEbay.Main.Api.Common.Services.BetServices
     public class BetService : IBetService
     {
         private readonly IPurchesDbService _purchesDbService;
-        private readonly IProductDbService _productDb;
+        private readonly IProductDbService _productDbService;
+
+        public BetService(IPurchesDbService purchesDbService,
+            IProductDbService productDbService)
+        {
+            _purchesDbService = purchesDbService;
+            _productDbService = productDbService;
+        }
 
         public async Task<ResponseData> PlaceBet(int userId, int productId)
         {
-            Product product = _productDb.Get(productId).Result;
+            Product product = _productDbService.Get(productId).Result;
 
-            Purchase purchase = new() { PurchasePrice= product.Price, PoductId = productId, UserId = userId, PurchaseStateId = product.PurchaseTypeId==1?2:1 };//1-sale, 2-auction
+            Purchase purchase = new() { PurchasePrice = product.Price, PoductId = productId, UserId = userId, PurchaseStateId = product.PurchaseTypeId == 1 ? 2 : 1 };//1-sale, 2-auction
             await _purchesDbService.Add(purchase);
 
             if (product.PurchaseTypeId == 1) //Коли одразу купляємо
             {
                 product.IsActive = false;
-                await _productDb.Update(product);
+                await _productDbService.Update(product);
             }
             else //Коли ставимо ставку
             {
                 product.Price = product.Price * (decimal)1.05;
-                await _productDb.Update(product);
+                await _productDbService.Update(product);
             }
 
             return ResponseData.Ok();
--- StepEbay.Api/Controllers/BetController.cs
+++ StepEbay.Api/Controllers/BetController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using StepEbay.Common.Models.RefitModels;
 using StepEbay.Data.Common.Services.BetsDbServices;
 using StepEbay.Main.Api.Common.Services.BetServices;
+using System.Security.Claims;
 
 namespace StepEbay.Main.Api.Controllers
 {
@@ -10,18 +11,18 @@ namespace StepEbay.Main.Api.Controllers
     [Route("bet")]
     public class BetController : ControllerBase
     {
-        public readonly IBetService _bets;
+        private readonly IBetService _bets;
 
         public BetController(IBetService bets)
         {
             _bets = bets;
         }
 
-        [HttpPost("place/{lotId}")]
-        public async Task<ResponseData> PlaceBet(int lotId)
+        [HttpPost("place/{lotid}")]
+        public async Task<ResponseData> PlaceBet(int lotid)
         {
-            int userId = int.Parse(User.Claims.Single(c => c.Type == "nickName").Value);
-            return await _bets.PlaceBet(userId, lotId);
+            int userId = int.Parse(User.Claims.Single(c => c.Type == ClaimTypes.Name).Value);
+            return await _bets.PlaceBet(userId, lotid);
         }
     }
 }
