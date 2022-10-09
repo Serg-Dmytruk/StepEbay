bafefe228bfb493dadda153726d84b3d563ac94c
122tp12 <63251202+122tp12@users.noreply.github.com>
2022-10-09 16:58:09 +0300

    Збільшення ціни, коли ставимо ставку

--- StepEbay.Api.Common/Services/BetServices/BetService.cs
+++ StepEbay.Api.Common/Services/BetServices/BetService.cs
@@ -20,14 +20,19 @@ namespace StepEbay.Main.Api.Common.Services.BetServices
         {
             Product product = _productDb.Get(productId).Result;
 
-            if (product.PurchaseTypeId == 1)
+            Purchase purchase = new() { PurchasePrice= product.Price, PoductId = productId, UserId = userId, PurchaseStateId = product.PurchaseTypeId==1?2:1 };//1-sale, 2-auction
+            await _purchesDbService.Add(purchase);
+
+            if (product.PurchaseTypeId == 1) //Коли одразу купляємо
             {
                 product.IsActive = false;
                 await _productDb.Update(product);
             }
-
-            Purchase purchase = new() { PoductId = productId, UserId = userId, PurchaseStateId = product.PurchaseTypeId==1?2:1 };
-            await _purchesDbService.Add(purchase);
+            else //Коли ставимо ставку
+            {
+                product.Price = product.Price * (decimal)1.05;
+                await _productDb.Update(product);
+            }
 
             return ResponseData.Ok();
         }
