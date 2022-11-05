63c36f8eb849fc14f50cc1b4d5d1912033dd61fb
aglaophot <aglaophot@gmail.com>
2022-11-05 20:36:02 +0200

    ch1

--- Verstka/StarsAndHeart/StarsAndHeart.js
+++ Verstka/StarsAndHeart/StarsAndHeart.js
@@ -1,38 +1,38 @@
 document.addEventListener("DOMContentLoaded", ready);
 function ready(){
-    stars();
+    let stars = document.getElementById("starsRatingBlock");
     heart();
 }
 
-function stars(){
-    let stars = document.getElementById("starsRatingBlock");
-    stars.addEventListener('mousemove', (event) => {
-        var rect = event.target.getBoundingClientRect();
-        var x = event.clientX - rect.left;  
-        let procentX=0;
-        if (x>100) procentX=100;
-        else if (x>75) procentX=80;
-        else if (x>50) procentX=60;
-        else if (x>25) procentX=40;
-        else if (x>0) procentX=20;  
+function MM(event){
+    var rect = event.target.getBoundingClientRect();
+    var x = event.clientX - rect.left;  
+    let procentX=0;
+    if (x>100) procentX=100;
+    else if (x>75) procentX=80;
+    else if (x>50) procentX=60;
+    else if (x>25) procentX=40;
+    else if (x>0) procentX=20;  
 
-        var style = stars.style;
-        style.setProperty('--starsRatingBlockWidth', procentX+'%');
-    });
-    stars.addEventListener('mouseleave', (event) => {
-        var style = stars.style;
-        style.setProperty('--starsRatingBlockWidth', '0%');
-    });
-
-    stars.addEventListener('mouseup', (event) => {
-        var style = stars.style;
-        style.setProperty('--starsRatingBG', "#716B6B");
-    });
-    stars.addEventListener('mousedown', (event) => {
-        var style = stars.style;
-        style.setProperty('--starsRatingBG', "#8BC65D");
-    });
+    var style = stars.style;
+    style.setProperty('--starsRatingBlockWidth', procentX+'%');
 }
+
+function ML() {
+    var style = stars.style;
+    style.setProperty('--starsRatingBlockWidth', '0%');
+}
+
+function MU() {
+    var style = stars.style;
+    style.setProperty('--starsRatingBG', "#716B6B");
+}
+
+function MD(){
+    var style = stars.style;
+    style.setProperty('--starsRatingBG', "#8BC65D");
+}
+
 
 
 function heart(){
