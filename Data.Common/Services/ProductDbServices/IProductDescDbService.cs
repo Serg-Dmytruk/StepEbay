d411543c4eca19b99414d98b59599d62b513c797
122tp12 <63251202+122tp12@users.noreply.github.com>
2022-11-16 19:06:05 +0200

    db serv

---
+++ Data.Common/Services/ProductDbServices/IProductDescDbService.cs
@@ -0,0 +1,10 @@
+﻿using StepEbay.Data.Common.Services.Default;
+using StepEbay.Data.Models.Products;
+
+namespace StepEbay.Data.Common.Services.ProductDbServices
+{
+    public interface IProductDescDbService : IDefaultDbService<int, ProductDesc>
+    {
+        public Task<ProductDesc> GetProductDescByProductId(int productId);
+    }
+}
